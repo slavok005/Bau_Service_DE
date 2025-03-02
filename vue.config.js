0533773976fe9e017c6e50d895f77cd9f698ec87
Slava <slavok.v005@mail.ru>
2025-03-02 13:52:30 +0100

    New

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,20 @@
+const webpack = require('webpack');
+
 module.exports = {
   configureWebpack: {
     plugins: [
-      new (require('webpack')).DefinePlugin({
-        '__VUE_OPTIONS_API__': true,
-        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false
+      new webpack.DefinePlugin({
+        '__VUE_OPTIONS_API__': JSON.stringify(true),
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
       })
     ]
+  },
+  devServer: {
+    allowedHosts: "all",
+    host: "0.0.0.0",
+    port: process.env.PORT || 10000,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
   }
 };
